14e5ac7c8ad7ac6a3de9cd8005a8a989e8566cec
Drew Dowling <drewdowling37@gmail.com>
2019-09-28 17:00:59 -0700

    green box moves around a big view box

--- src/Main.elm
+++ src/Main.elm
@@ -1,41 +1,147 @@
+module Main exposing (Model, Msg(..), init, main, update, view)
+
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Debug exposing (log)
+import Html exposing (..)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import Time
 
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
+
 
 
 -- MODEL
 
-type alias Model = Int
 
-init : Model
-init =
-  0
+type alias Model =
+    { food : Position
+    , snake : List Position
+    , direction : Direction
+    }
+
+
+type Direction
+    = Up
+    | Down
+    | Left
+    | Right
+    | None
+
+
+type alias Position =
+    { x : Int, y : Int }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { food = { x = 5, y = 3 }
+      , snake = [ { x = 0, y = 0 } ]
+      , direction = None
+      }
+    , Cmd.none
+    )
+
 
 
 -- UPDATE
 
-type Msg = Increment | Decrement
 
-update : Msg -> Model -> Model
+type Msg
+    = Tick Time.Posix
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    let
+        _ =
+            log "model" model
+    in
+    case msg of
+        Tick _ ->
+            -- move the food around the grid
+            let
+                nextX =
+                    modBy gridWidth (model.food.x + 1)
 
-    Decrement ->
-      model - 1
+                nextY =
+                    modBy gridHeight (model.food.y + 1)
+            in
+            ( { model | food = { x = nextX, y = nextY } }
+            , Cmd.none
+            )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every 300 Tick
+
 
 
 -- VIEW
 
+
+cellSize =
+    10
+
+
+gridWidth =
+    20
+
+
+gridHeight =
+    15
+
+
+gridWidthPixels =
+    gridWidth * cellSize
+
+
+gridHeightPixels =
+    gridHeight * cellSize
+
+
+cellSizeStr =
+    String.fromInt cellSize
+
+
+gridWidthStr =
+    String.fromInt gridWidthPixels
+
+
+gridHeightStr =
+    String.fromInt gridHeightPixels
+
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
+    svg
+        [ class "grid"
+        , viewBox ("0 0 " ++ gridWidthStr ++ " " ++ gridHeightStr)
+        ]
+        (renderBackground ++ renderCell model.food)
+
+
+renderBackground =
+    [ rect
+        [ width gridWidthStr
+        , height gridHeightStr
+        , fill "#997B1F"
+        ]
+        []
+    ]
+
+
+renderCell position =
+    [ rect
+        [ x (String.fromInt (position.x * cellSize))
+        , y (String.fromInt (position.y * cellSize))
+        , width cellSizeStr
+        , height cellSizeStr
+        , fill "green"
+        , stroke "gray"
+        ]
+        []
     ]
